b3ee758dbbf0eb488f1855e8c97719fb8baea34c
toni <toni_hp@hotmail.com>
2020-07-27 20:42:13 +0200

    Updates

---
+++ precache-manifest.dd3499ebdbc1d6861fc48da6ef5876a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f764812617be4d685dd830f4276e0f0e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3d24d03e795329dfab08",
+    "url": "/monsters-rolodex/static/css/main.64ef2191.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d24d03e795329dfab08",
+    "url": "/monsters-rolodex/static/js/main.038d3d8b.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
